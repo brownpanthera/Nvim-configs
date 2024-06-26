c2ed18baa9964609faf2ba4610564da3e5609bfb
Ali Faizan <68064523+brownpanthera@users.noreply.github.com>
2024-06-26 11:46:12 +0530

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,71 @@
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.o.autoindent = true
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- lazy.nvim 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- plugins
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", lazy = true, priority = 1000 },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  { "kyazdani42/nvim-tree.lua" },
+  { "jose-elias-alvarez/null-ls.nvim" },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+  },
+  -- Tree-sitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "python", "go" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+      })
+    end,
+  },
+
+}
+
+local opts = {}
+require("lazy").setup(plugins, opts)
+
+-- Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-n> ', ':Neotree filesystem reveal left<CR>', {})
+
+-- Catppuccin
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
